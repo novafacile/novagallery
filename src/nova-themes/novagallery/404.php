39452597fb90a4d0cb7ffcc77b7f55429df97389
David <43939617+david-novafacile@users.noreply.github.com>
2021-10-19 00:21:50 +0200

    fix wording

--- src/nova-themes/novagallery/404.php
+++ src/nova-themes/novagallery/404.php
@@ -1,7 +1,7 @@
 <div class="jumbotron jumbotron-fluid bg-transparent">
   <div class="container">
     <h1 class="display-4"><?php L::p('404 / Page not found'); ?></h1>
-    <p class="lead mt-4 mb-4"><?php L::p('Sorry, this page does not seem to be available.'); ?></p>
+    <p class="lead mt-4 mb-4"><?php L::p('Sorry, this page does not seem to exist.'); ?></p>
     <p class="lead"><a href="/" class="text-white link-back">&laquo; <?php L::p('Back to Home'); ?></a></p>
   </div>
 </div>
