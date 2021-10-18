44338ffe7f438e33351a43d3bb9eae0d0c352b33
David <43939617+david-novafacile@users.noreply.github.com>
2021-10-19 00:06:16 +0200

    show 404 error if image not found

--- src/nova-base/app/image.php
+++ src/nova-base/app/image.php
@@ -5,7 +5,7 @@
 
 // pass only allowed sizes
 if(in_array($size, Site::config('allowedImageSizes'))){
-  Image::render($album, $image, $size, Site::config('imageCache'), false);
+  Image::render($album, $image, $size, Site::config('imageCache'), true);
 } else {
   header('HTTP/1.0 404 Not Found');
 }
