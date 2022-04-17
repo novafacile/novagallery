f50c5e52a8084c09faf9609904da9a27c3abd42a
David <43939617+david-novafacile@users.noreply.github.com>
2022-04-17 22:47:24 +0200

    fix image url in subdir

--- src/nova-base/lib/Image.php
+++ src/nova-base/lib/Image.php
@@ -54,18 +54,22 @@ class Image {
    * @return string URL - url of (resized) image
    ************/
   public static function url($album, $image, $size = false){
-
     // split album name if is in sub dir because slash should't be encoded with rawurlencode
     if(strpos($album, '/')){
       $pathArray = explode('/', $album);
-      $album = array_pop($pathArray); // remove last entry from array because, it's the final album
-      $album = rawurlencode($album);
+      $path = '';
       foreach ($pathArray as $value) {
-        $album = rawurlencode($value).'/'.$album;
+        if($path){
+          $path = rawurlencode($value);
+        } else {
+          $path = $path.'/'.rawurlencode($value);
+        }
       }
     } else {
       $album = rawurlencode($album);
     }
+
+    
 
     // split image name if is in sub dir because contains sub dirs
     if(strpos($image, '/')){
