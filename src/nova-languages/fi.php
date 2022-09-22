cc8eb28b882cab8b1f544cebd61b768d9eb3a7ae
David <43939617+david-novafacile@users.noreply.github.com>
2022-09-22 21:34:14 +0200

    add finish language
    Thanks a lot @ Jonne

---
+++ src/nova-languages/fi.php
@@ -0,0 +1,10 @@
+<?php defined("NOVA") or die(); ?>
+{
+  "login": "Kirjaudu sisään",
+  "logout": "Kirjaudu ulos",
+  "password": "Salasana",
+  "back": "Takaisin",
+  "404-/-page-not-found": "404 / Sivua ei löydy",
+  "sorry-this-page-does-not-seem-to-exist": "Pahoittelut, hakemaaasi sivua ei löydy palvelimelta",
+  "back-to-home": "Takaisin kotisivulle"
+}
